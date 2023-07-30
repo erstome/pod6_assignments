d592f878096cec7c4bda49140ad0c7f0caad8b5b
Emanuel Tomé <emanuel.tome@fc.up.pt>
2023-07-30 16:27:44 +0100

    refactoring

--- assignments/life_expectancy/cleaning.py
+++ assignments/life_expectancy/cleaning.py
@@ -1,26 +1,23 @@
 """aaa
 """
 # Imports
+import argparse
 import os
 import pandas as pd
 
 # Functions
-def clean_data() -> pd.DataFrame:
-    """_summary_
-
-    Returns
-    -------
-    _type_
-        _description_
+def clean_data(region: str = 'PT') -> None:
     """
-    
+    Function to import, clean and save data.
+    Saves to a *.csv file.
+    """
     # File path
     file_path = os.path.join(os.path.dirname(__file__),
                              './data/')
-    
+
     # Loads the eu_life_expectancy_raw.tsv data from the data folder.
     data = pd.read_csv(file_path + 'eu_life_expectancy_raw.tsv', sep='\t')
-    
+
     # Unpivots the date to long format, so that we have the following columns:
     # unit, sex, age, region, year, value.
     data = pd.concat(
@@ -34,7 +31,7 @@ def clean_data() -> pd.DataFrame:
 
     data = pd.melt(data, id_vars=data.columns[0:4], value_vars=data.columns[4:],
                    var_name='year')
-    
+
     # Ensures year is an int (with the appropriate data cleaning if required)
     data = data.astype({'year': 'int'})
 
@@ -50,13 +47,19 @@ def clean_data() -> pd.DataFrame:
     data = data.astype({'value': 'float'})
 
     # Filters only the data where region equal to PT (Portugal).
-    data = data[data.region=='PT']
+    data = data[data.region==region]
 
-    # Save the resulting data frame to the data folder as pt_life_expectancy.csv. 
+    # Save the resulting data frame to the data folder as pt_life_expectancy.csv.
     # Ensure that no numerical index is saved.
-    data.to_csv(file_path + 'pt_life_expectancy.csv', index=False)
+    data.to_csv(file_path + region.lower() + '_life_expectancy.csv', index=False)
 
 
-if __name__ == '__main__':
-    clean_data()
+if __name__ == '__main__': # pragma: no cover
+    # parser
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--region', type=str, required=False, default='PT')
+    args = parser.parse_args()
+
+    # function call
+    clean_data(region=args.region)
     
