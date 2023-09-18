c46789489001622643b799d56cb240a26ef2787e
Emanuel Tomé <emanuel.tome@fc.up.pt>
2023-09-18 22:56:03 +0100

    tes_main_data with fixture for load_data function

--- life_expectancy/tests/test_cleaning.py
+++ life_expectancy/tests/test_cleaning.py
@@ -10,12 +10,8 @@ def test_main_data(patched_load_data, pt_life_expectancy_expected, eu_life_expec
     """Run the `clean_data` function and compare the output to the expected output"""
 
     patched_load_data.return_value = eu_life_expectancy_raw
-    pt_life_expectancy_actual = main(region='PT')
-
-    print(pt_life_expectancy_actual.head())
-    print(pt_life_expectancy_expected.head())
+    pt_life_expectancy_actual = main(region='PT').reset_index(drop=True)
 
     pd.testing.assert_frame_equal(
         pt_life_expectancy_actual, pt_life_expectancy_expected
     )
-
