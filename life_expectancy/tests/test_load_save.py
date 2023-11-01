47e59a6cc216c324f3c343de1941746432612822
Emanuel Tomé <emanuel.tome@fc.up.pt>
2023-11-01 23:42:59 +0000

    Design pattern Strategy Pattern

--- life_expectancy/tests/test_load_save.py
+++ life_expectancy/tests/test_load_save.py
@@ -12,15 +12,26 @@ from . import FIXTURES_DIR
 # Functions to be tested
 from life_expectancy.life_expectancy.load_save import load_data
 from life_expectancy.life_expectancy.load_save import save_data
+from life_expectancy.life_expectancy.load_save import TSVDataFormat, JSONDataFormat
 
-def test_load_data(eu_life_expectancy_raw):
-    """Tests function load_data"""
+def test_load_data_tsv(eu_life_expectancy_raw):
+    """Tests function load_data tsv files"""
     data_directory = str(Path(FIXTURES_DIR, 'eu_life_expectancy_raw.tsv'))
-    eu_life_expectancy_raw_actual = load_data(data_directory)
+    eu_life_expectancy_raw_actual = load_data(data_directory, 
+                                              data_format_strategy=TSVDataFormat())
 
     pd.testing.assert_frame_equal(
         eu_life_expectancy_raw_actual, eu_life_expectancy_raw
     )
+
+def test_load_data_json():
+    """Tests function load_data for json files"""
+    data_directory = str(Path(FIXTURES_DIR, 'eurostat_life_expect_sample.json'))
+    eu_life_expectancy_raw_actual = load_data(data_directory, 
+                                              data_format_strategy=JSONDataFormat())
+    
+    assert eu_life_expectancy_raw_actual.shape[0]==100
+
 
 @patch('life_expectancy.life_expectancy.load_save.pd.DataFrame.to_csv')
 def test_save_data(patched_DataFrame_to_csv, eu_life_expectancy_expected):
