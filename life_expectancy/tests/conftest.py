7981eaabd803423d364fc205b59b46024873d698
Emanuel Tomé <emanuel.tome@fc.up.pt>
2023-11-02 00:04:07 +0000

    improve unit test for json files

--- life_expectancy/tests/conftest.py
+++ life_expectancy/tests/conftest.py
@@ -1,4 +1,5 @@
 """Pytest configuration file"""
+import json
 import pandas as pd
 import pytest
 
@@ -26,6 +27,12 @@ def pt_life_expectancy_expected() -> pd.DataFrame:
 def eu_life_expectancy_raw() -> pd.DataFrame:
     """Fixture to load a sample of the raw data"""
     return pd.read_csv(FIXTURES_DIR / "eu_life_expectancy_raw.tsv", sep='\t')
+
+@pytest.fixture(scope="session")
+def eurostat_life_expect_raw() -> pd.DataFrame:
+    """Fixture to load a sample of the raw data"""
+    with open(FIXTURES_DIR / "eurostat_life_expect_sample.json", encoding="utf-8") as file:
+        return pd.DataFrame(json.load(file))
 
 @pytest.fixture(scope="session")
 def eu_life_expectancy_expected() -> pd.DataFrame():
